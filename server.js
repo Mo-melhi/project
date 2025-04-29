683a38a63bb6a8e54e45182899f6160a066d97d2
Mohammed Omar <m8o9h7.gg5@gmail.com>
2025-04-30 02:19:32 +0300

    meow4

--- server.js
+++ server.js
@@ -254,7 +254,7 @@ app.get('/branches/hours', async (req, res) => {
 
   
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.DATABASE_URL|| 3007;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
